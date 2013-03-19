cf09f99cb7253397ebe23233340c33af1ef023c4
Simon Roth <git@simonroth.ch>
2013-03-19 10:09:58 +0100

    add import function

--- main.m
+++ main.m
@@ -3,3 +3,19 @@
 %                Labor II
 % Version:       March 2013
 % Last revision: 19. March 2013
+
+clear all
+close all
+
+
+% import data
+% -------------------------------------------------------------------------
+
+path             = 'data/2012/';
+lysimeter_folder = 'lysimeter/';
+lysimeter_file   = '2012_lysimeter_01.txt';
+meteo_folder     = 'meteo/';
+meteo_file       = '2012_meteodata_data.txt';
+
+lysimeter = dlmread( [ path lysimeter_folder lysimeter_file ] );
+meteo     = dlmread( [ path meteo_folder meteo_file ], ';', 3, 1 );
