ea92ab6ea098e34aea85bac512b329931bed781b
Simon Roth <git@simonroth.ch>
2013-03-26 23:15:40 +0100

    i'm tired...

--- code/main.m
+++ code/main.m
@@ -30,7 +30,6 @@ meteo.data             = [ lysimeter.data( :, 1 ), meteo.data( 1:end-1, 2:end ) 
 
 
 %% Analysis specification
-t          = { '01.01.2012', '31.12.2012' };	% [dd.mm.yyyy]
 plant      = 'Raps';
 cropFactor = 1.15;                              % [-]
 
@@ -83,9 +82,9 @@ Tair.mMin = monthlyMean( Tair.dMin, time.d );
 Tair.m    = ( Tair.mMax+Tair.mMin )/2;
 
 % precipitation [mm]
-percip.h = meteo.data( :, 9 );
-percip.d = dailySum( percip.h );
-percip.m = monthlySum( percip.h, time.h );
+precip.h = meteo.data( :, 9 );
+precip.d = dailySum( precip.h );
+precip.m = monthlySum( precip.h, time.h );
 
 % relative humidity [%]
 RH.h = meteo.data( :, 10 );
@@ -107,9 +106,12 @@ es.m = ( satVapPressure( Tair.mMax )+satVapPressure( Tair.mMin ) )/2;
 %% Compute evapotranspiration
 
 % actual evapotranspiration [mm]
-AET.h = percip.h-percol.h-storage.h;
-AET.d = percip.d-percol.d-storage.d;
-AET.m = percip.m-percol.m-storage.m;
+AET.h              = precip.h-percol.h-storage.h;
+AET.h( AET.h < 0 ) = 0;
+AET.d              = precip.d-percol.d-storage.d;
+AET.d( AET.d < 0 ) = 0;
+AET.m              = precip.m-percol.m-storage.m;
+AET.m( AET.m < 0 ) = 0;
 
 % Penman-Monteith [mm]
 penMonPET.d = penmanMonteith( es.d, Tair.d, RH.d, alpha, Rs.d, time.d, latitude, Gsc, hGeo, sigma, Tair.dMax, Tair.dMin, press.d, windSp.d )*cropFactor;
@@ -133,10 +135,41 @@ turcIvanovPET.m = [ ivanovPET.m( 1:2 ); turcPET.m( 3:10 ); ivanovPET.m( 11:12 ) 
 % labels for the data vector as strings separated by commas }, the period,
 % the titel of the plot )
 
-niceFigure( time.d, [ AET.d, penMonPET.d ], { 'AET', 'Penman-Monteith' }, t, [ plant ' (daily)' ] )
-niceFigure( time.m, [ AET.m, penMonPET.m ], { 'AET', 'Penman-Monteith' }, t, [ plant ' (monthly)' ] )
-niceFigure( time.d, [ penMonPET.d, turcPET.d, ivanovPET.d, turcIvanovPET.d ], { 'Penman-Monteith', 'Turc', 'Ivanov', 'Turc and Ivanov combined' }, t, [ plant ' (daily)' ] )
-niceFigure( time.m, [ penMonPET.m, turcPET.m, ivanovPET.m, turcIvanovPET.m ], { 'Penman-Monteith', 'Turc', 'Ivanov', 'Turc and Ivanov combined' }, t, [ plant ' (monthly)' ] )
+% AET + Precip, Tair, Rs, RH (week hourly)
+t = { '01.04.2012', '08.04.2012' };     % [dd.mm.yyyy]
+niceFigure( time.h, AET.h, time.h, precip.h, 'stairs', 'stairs', t, 'Datum', 'AET [mm/h]', 'Niederschlag [mm/h]' )
+niceFigure( time.h, AET.h, time.h, Tair.h, 'stairs', 'plot', t, 'Datum', 'AET [mm/h]', 'Temperatur [°C]' )
+niceFigure( time.h, AET.h, time.h, Rs.h, 'stairs', 'plot', t, 'Datum', 'AET [mm/h]', 'Sonneneinstrahlung [W/m2]' )
+niceFigure( time.h, AET.h, time.h, RH.h, 'stairs', 'plot', t, 'Datum', 'AET [mm/h]', 'Relative Luftfeuchtigkeit [%]' )
+
+% AET + Precip, Tair, Rs, RH (year monthly)
+t = { '01.01.2012', '31.12.2012' };     % [dd.mm.yyyy]
+niceFigure( time.m, AET.m, time.m, precip.m, 'stairs', 'stairs', t, 'Datum', 'AET [mm/m]', 'Niederschlag [mm/m]' )
+niceFigure( time.m, AET.m, time.m, Tair.m, 'stairs', 'plot', t, 'Datum', 'AET [mm/m]', 'Temperatur [°C]' )
+niceFigure( time.m, AET.m, time.m, Rs.m, 'stairs', 'plot', t, 'Datum', 'AET [mm/m]', 'Sonneneinstrahlung [W/m2]' )
+niceFigure( time.m, AET.m, time.m, RH.m, 'stairs', 'plot', t, 'Datum', 'AET [mm/m]', 'Relative Luftfeuchtigkeit [%]' )
+
+% Penman-Monteith, TurcIvanov + Precip, Tair, Rs, RH (week daily)
+t = { '01.04.2012', '08.04.2012' };     % [dd.mm.yyyy]
+niceFigure( time.d, [ penMonPET.d, turcIvanovPET.d ], time.d, precip.d, 'stairs', 'stairs', t, 'Datum', 'PET [mm/d]', 'Niederschlag [mm/d]' )
+niceFigure( time.d, [ penMonPET.d, turcIvanovPET.d ], time.h, Tair.h, 'stairs', 'plot', t, 'Datum', 'PET [mm/d]', 'Temperatur [°C]' )
+niceFigure( time.d, [ penMonPET.d, turcIvanovPET.d ], time.h, Rs.h, 'stairs', 'plot', t, 'Datum', 'PET [mm/d]', 'Sonneneinstrahlung [W/m2]' )
+niceFigure( time.d, [ penMonPET.d, turcIvanovPET.d ], time.h, RH.h, 'stairs', 'plot', t, 'Datum', 'PET [mm/d]', 'Relative Luftfeuchtigkeit [%]' )
+
+% Penman-Monteith, TurcIvanov + Precip, Tair, Rs, RH (year mothly)
+t = { '01.01.2012', '31.12.2012' };     % [dd.mm.yyyy]
+niceFigure( time.m, [ penMonPET.m, turcIvanovPET.m ], time.m, precip.m, 'stairs', 'stairs', t, 'Datum', 'PET [mm/m]', 'Niederschlag [mm/m]' )
+niceFigure( time.m, [ penMonPET.m, turcIvanovPET.m ], time.m, Tair.m, 'stairs', 'plot', t, 'Datum', 'PET [mm/m]', 'Temperatur [°C]' )
+niceFigure( time.m, [ penMonPET.m, turcIvanovPET.m ], time.m, Rs.m, 'stairs', 'plot', t, 'Datum', 'PET [mm/m]', 'Sonneneinstrahlung [W/m2]' )
+niceFigure( time.m, [ penMonPET.m, turcIvanovPET.m ], time.m, RH.m, 'stairs', 'plot', t, 'Datum', 'PET [mm/m]', 'Relative Luftfeuchtigkeit [%]' )
+
+% AET + PET (week daily)
+t = { '01.04.2012', '08.04.2012' };     % [dd.mm.yyyy]
+niceFigure( time.d, AET.d, time.d, [ penMonPET.d, turcIvanovPET.d ], 'stairs', 'stairs', t, 'Datum', 'AET [mm/d]', 'PET [mm/d]' )
+
+% AET + PET (year monthly)
+t = { '01.01.2012', '31.12.2012' };     % [dd.mm.yyyy]
+niceFigure( time.m, AET.m, time.m, [ penMonPET.m, turcIvanovPET.m ], 'stairs', 'stairs', t, 'Datum', 'AET [mm/d]', 'PET [mm/d]' )
 
 
 %% Free memory
